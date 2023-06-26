4b85d0bd32f9ce14234fe43c4644105683d62edb
Ivan Trajanovski <ivantrajanovski@gmail.com>
2023-06-26 13:31:51 +0200

    first commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by ivan ruwido  on 26.06.23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
